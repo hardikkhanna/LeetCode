9487d5340757fdf9e1c86756a783adc3cddb49fd
Hardik khanna <khannahardik007@gmail.com>
2021-01-04 10:25:59 +0530

    Convert Binary Number in LinkedList to Integer

---
+++ Leetcode/src/leetcode/linkedlist/prolems/ConvertBinaryNumberInListToInteger.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package leetcode.linkedlist.prolems;
+
+/**
+ * @author NIPC-43
+ *
+ *         Date : Jan 4, 2021 Time : 9:41:43 AM
+ */
+public class ConvertBinaryNumberInListToInteger extends ScannerInput {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ListNode head = scannerInput();
+		int ans = getDecimalValue(head);
+		System.out.println(ans);
+	}
+
+	public static int getDecimalValue(ListNode head) {
+		int num = head.val;
+
+		while (head.next != null) {
+
+			num = num * 2 + head.next.val;
+			head = head.next;
+		}
+		return num;
+	}
+
+}
---
+++ Leetcode/src/leetcode/linkedlist/prolems/ListNode.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package leetcode.linkedlist.prolems;
+
+/**
+ * @author NIPC-43
+ *
+ *         Date : Jan 4, 2021 Time : 9:48:27 AM
+ */
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode() {
+	}
+
+	ListNode(int val) {
+		this.val = val;
+	}
+
+	ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
+}
