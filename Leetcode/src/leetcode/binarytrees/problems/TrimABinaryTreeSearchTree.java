bc67437887ddf918081e3500359bdfbbe08a5b9a
Hardik khanna <khannahardik007@gmail.com>
2021-01-21 13:48:53 +0530

    Palindrome String

---
+++ Leetcode/src/leetcode/binarytrees/problems/TrimABinaryTreeSearchTree.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package leetcode.binarytrees.problems;
+
+/**
+ * @author NIPC-43
+ *
+ *         Date : Jan 20, 2021 Time : 5:37:30 PM
+ */
+public class TrimABinaryTreeSearchTree extends BinaryTreeUse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = takeInputLevelWise();
+		//root = 
+	}
+
+}
---
+++ Leetcode/src/leetcode/string/problems/ReverseFrontBackString.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package leetcode.string.problems;
+
+/**
+ * @author NIPC-43
+ *
+ *         Date : Jan 20, 2021 Time : 10:07:31 PM
+ */
+public class ReverseFrontBackString {
+
+	public static void main(String[] args) {
+		String str = "the big cat jumped over the lazy dog";
+		String ans = reverse(str.split(" "));
+		System.out.println(ans);
+
+	}
+
+	private static String reverse(String[] strings) {
+		int k = strings.length - 1;
+		StringBuilder ans = new StringBuilder();
+		for (int i = 0; i < strings.length / 2; i++) {
+			ans.append(new StringBuilder(strings[k] + " ").reverse().toString())
+					.append(new StringBuilder(strings[i] + " ").reverse().toString());
+			k--;
+		}
+		return strings.length % 2 != 0 ? ans.append(" " + strings[strings.length / 2]).toString().trim()
+				: ans.toString().trim();
+	}
+
+}
