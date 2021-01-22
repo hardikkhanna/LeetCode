ddf2a85c2bb0226e17f43674f5a073b47a52f77a
Hardik khanna <khannahardik007@gmail.com>
2021-01-22 12:53:41 +0530

    Two Sum in BST

---
+++ Leetcode/src/leetcode/binarytrees/problems/TwoSum.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package leetcode.binarytrees.problems;
+
+/**
+ * @author NIPC-43
+ *
+ *         Date : Jan 22, 2021 Time : 12:33:07 PM
+ */
+public class TwoSum extends BinaryTreeUse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = takeInputLevelWise();
+		int k = 9;
+		Boolean ans = findTarget(root, k);
+		System.out.println(ans);
+
+	}
+
+	/**
+	 * @param root
+	 * @param k
+	 * @return
+	 */
+	private static Boolean findTarget(BinaryTreeNode<Integer> root, int k) {
+		if (root == null)
+			return false;
+
+		return null;
+	}
+
+}
