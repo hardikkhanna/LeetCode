586bd31c8fffeb07396a50273af174e6830fc2b5
Hardik khanna <khannahardik007@gmail.com>
2021-01-22 12:31:05 +0530

    Convert a Sorted Array to BST

---
+++ Leetcode/src/leetcode/binarytrees/problems/ConvertSortedArrayToBST.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package leetcode.binarytrees.problems;
+
+import leetcode.array.problems.ScannerInput;
+
+/**
+ * @author NIPC-43
+ *
+ *         Date : Jan 22, 2021 Time : 11:43:13 AM
+ */
+public class ConvertSortedArrayToBST extends ScannerInput {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] arr = get1DArray();
+		BinaryTreeNode<Integer> root = sortedArrayToBST(arr);
+		BinaryTreeUse.printTreeLevelWise(root);
+	}
+
+	/**
+	 * @param arr
+	 * @return
+	 */
+	public static BinaryTreeNode<Integer> sortedArrayToBST(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return null;
+		}
+		return getTreeNode(nums, 0, nums.length - 1);
+	}
+
+	private static BinaryTreeNode<Integer> getTreeNode(int[] nums, int start, int end) {
+		if (start > end) {
+			return null;
+		}
+		int middle = start + (end - start) / 2;
+		BinaryTreeNode<Integer> n = new BinaryTreeNode<Integer>(nums[middle]);
+		n.left = getTreeNode(nums, start, middle - 1);
+		n.right = getTreeNode(nums, middle + 1, end);
+		return n;
+	}
+
+}
