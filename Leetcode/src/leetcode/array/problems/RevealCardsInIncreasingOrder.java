f9590caaaaa630f96ab90b08ba06cc75c970c6fa
Hardik khanna <khannahardik007@gmail.com>
2021-01-24 17:13:51 +0530

    Reveal Cards

---
+++ Leetcode/src/leetcode/array/problems/RevealCardsInIncreasingOrder.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package leetcode.array.problems;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author NIPC-43
+ *
+ *         Date : Jan 24, 2021 Time : 5:07:25 PM
+ */
+public class RevealCardsInIncreasingOrder extends ScannerInput {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] arr = get1DArray();
+		arr = deckRevealedIncreasing(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
+	/**
+	 * @param arr
+	 * @return
+	 */
+	public static int[] deckRevealedIncreasing(int[] deck) {
+		int n= deck.length;
+        Arrays.sort(deck);
+        Queue<Integer> q= new LinkedList<>();
+        for (int i=0; i<n; i++) q.add(i);
+        int[] res= new int[n];
+        for (int i=0; i<n; i++){
+            res[q.poll()]=deck[i];
+            q.add(q.poll());
+        }
+        return res;
+	}
+
+}
