33d27af234e7444aa2a97784ab4fd7aa897fa05e
Hardik khanna <khannahardik007@gmail.com>
2021-01-28 20:33:05 +0530

    Smallest String With A Given Numeric Value

---
+++ Leetcode/src/leetcode/binarytrees/problems/BalancedBinaryTree.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package leetcode.binarytrees.problems;
+
+/**
+ * @author NIPC-43
+ *
+ *         Date : Jan 27, 2021 Time : 5:58:04 PM
+ */
+public class BalancedBinaryTree extends BinaryTreeUse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = takeInputLevelWise();
+		Boolean ans = isBalanced(root);
+		System.out.println(ans);
+
+	}
+
+	private static Boolean isBalanced(BinaryTreeNode<Integer> root) {
+		if (root == null || (root.left == null && root.right == null))
+			return true;
+		int leftHeight = height(root.left);
+		int rightHeight = height(root.right);
+		return Math.abs(leftHeight - rightHeight) <= 1;
+
+	}
+
+	/**
+	 * @param left
+	 * @return
+	 */
+	private static int height(BinaryTreeNode<Integer> root) {
+		if (root == null) {
+			return 0;
+		}
+		int leftHeight = height(root.left);
+		int rightHeight = height(root.right);
+		return Math.max(leftHeight, rightHeight) + 1;
+	}
+
+}
---
+++ Leetcode/src/leetcode/string/problems/GetNumericString.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package leetcode.string.problems;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author NIPC-43
+ *
+ *         Date : Jan 28, 2021 Time : 2:27:09 PM
+ */
+public class GetNumericString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		String str = getSmallestString(n, k);
+		System.out.println(str);
+		sc.close();
+
+	}
+
+	/**
+	 * @param n
+	 * @param k
+	 * @return
+	 */
+	private static String getSmallestString(int n, int k) {
+		char[] chars = new char[n];
+		Arrays.fill(chars, 'a');
+		k -= n;
+		while (k > 0) {
+			if (chars[n - 1] < 'z') {
+				chars[n - 1]++;
+				k--;
+			} else {
+				n--;
+			}
+		}
+		return new String(chars);
+	}
+
+}
