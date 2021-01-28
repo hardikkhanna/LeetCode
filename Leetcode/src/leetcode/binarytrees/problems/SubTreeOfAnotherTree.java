096d88856b4c1f5fab2bfbbad7523c07156cf1e4
Hardik khanna <khannahardik007@gmail.com>
2021-01-28 22:25:22 +0530

    Subtree of Root Tree

---
+++ Leetcode/src/leetcode/binarytrees/problems/SubTreeOfAnotherTree.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package leetcode.binarytrees.problems;
+
+/**
+ * @author NIPC-43
+ *
+ *         Date : Jan 28, 2021 Time : 10:23:15 PM
+ */
+public class SubTreeOfAnotherTree extends BinaryTreeUse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = takeInputLevelWise();
+		BinaryTreeNode<Integer> root2 = takeInputLevelWise();
+		Boolean ans = isSubtree(root, root);
+		System.out.println(ans);
+
+	}
+
+	/**
+	 * @param root
+	 * @param root2
+	 * @return
+	 */
+	public boolean isSubtree(BinaryTreeNode<Integer> s, BinaryTreeNode<Integer> t) {
+		if (s == null)
+			return false;
+		if (isSame(s, t))
+			return true;
+		return isSubtree(s.left, t) || isSubtree(s.right, t);
+	}
+
+	private boolean isSame(BinaryTreeNode<Integer> s, BinaryTreeNode<Integer> t) {
+		if (s == null && t == null)
+			return true;
+		if (s == null || t == null)
+			return false;
+
+		if (s.data != t.data)
+			return false;
+
+		return isSame(s.left, t.left) && isSame(s.right, t.right);
+	}
+
+}
