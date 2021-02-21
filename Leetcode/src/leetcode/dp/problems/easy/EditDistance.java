1f369108c6e3df2ccb99aa547338569b843b7b22
Hardik khanna <khannahardik007@gmail.com>
2021-02-21 22:35:27 +0530

    Print possible ways to edit distance

--- Leetcode/src/leetcode/dp/problems/easy/EditDistance.java
+++ Leetcode/src/leetcode/dp/problems/easy/EditDistance.java
@@ -12,6 +12,8 @@ import java.util.Scanner;
  */
 public class EditDistance {
 
+	static int[][] DP;
+
 	/**
 	 * @param args
 	 */
@@ -21,6 +23,7 @@ public class EditDistance {
 		String word2 = sc.next();
 		int operations = editDistance(word1, word2);
 		System.out.println(operations);
+		printPossibleWays(word1, word2);
 		sc.close();
 	}
 
@@ -44,7 +47,32 @@ public class EditDistance {
 					dp[i][j] = 1 + Math.min(dp[i][j - 1], Math.min(dp[i - 1][j - 1], dp[i - 1][j]));
 			}
 		}
+		DP = dp;
 		return dp[m][n];
+	}
+
+	/**
+	 * 
+	 */
+	private static void printPossibleWays(String word1, String word2) {
+		int i = word1.length();
+		int j = word2.length();
+		while (i != 0 && j != 0) {
+			if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+				i--;
+				j--;
+			} else if (DP[i][j] == DP[i - 1][j - 1] + 1) { // Replace the character
+				System.out.println("Replaced " + word1.charAt(i - 1) + " to " + word2.charAt(j - 1));
+				i--;
+				j--;
+			} else if (DP[i][j] == DP[i - 1][j] + 1) {
+				System.out.println("Delete	d " + word1.charAt(i - 1));
+				i--;
+			} else if (DP[i][j] == DP[i][j - 1] + 1) {
+				System.out.println("Inserted " + word2.charAt(j - 1));
+				j--;
+			}
+		}
 	}
 
 }
